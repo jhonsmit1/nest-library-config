ada465ddc26397b47fdbc43b1f9f1b78e5009353
jhonsmit1 <realtube565@gmail.com>
2026-03-02 15:04:43 -0500

    change library

--- dist/config/app/app-config.module.js
+++ dist/config/app/app-config.module.js
@@ -5,32 +5,22 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var AppConfigModule_1;
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppConfigModule = void 0;
 const common_1 = require("@nestjs/common");
-const config_1 = require("@nestjs/config");
-const env_schema_1 = require("../env.schema");
 const app_config_service_1 = require("./app-config.service");
-let AppConfigModule = class AppConfigModule {
+let AppConfigModule = AppConfigModule_1 = class AppConfigModule {
+    static forRoot() {
+        return {
+            module: AppConfigModule_1,
+            providers: [app_config_service_1.AppConfigService],
+            exports: [app_config_service_1.AppConfigService],
+        };
+    }
 };
 exports.AppConfigModule = AppConfigModule;
-exports.AppConfigModule = AppConfigModule = __decorate([
+exports.AppConfigModule = AppConfigModule = AppConfigModule_1 = __decorate([
     (0, common_1.Global)(),
-    (0, common_1.Module)({
-        imports: [
-            config_1.ConfigModule.forRoot({
-                isGlobal: true,
-                envFilePath: [".env.local", ".env"],
-                validate: (config) => {
-                    const result = env_schema_1.envSchema.safeParse(config);
-                    if (!result.success) {
-                        throw new Error("Invalid environment configuration");
-                    }
-                    return result.data;
-                },
-            }),
-        ],
-        providers: [app_config_service_1.AppConfigService],
-        exports: [app_config_service_1.AppConfigService],
-    })
+    (0, common_1.Module)({})
 ], AppConfigModule);
--- src/config/app/app-config.module.ts
+++ src/config/app/app-config.module.ts
@@ -1,25 +1,14 @@
-import { Global, Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
-import { envSchema } from "../env.schema";
+import { Global, Module, DynamicModule } from "@nestjs/common";
 import { AppConfigService } from "./app-config.service";
 
-
 @Global()
-@Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      envFilePath: [".env.local", ".env"],
-      validate: (config) => {
-        const result = envSchema.safeParse(config);
-        if (!result.success) {
-          throw new Error("Invalid environment configuration");
-        }
-        return result.data;
-      },
-    }),
-  ],
-  providers: [AppConfigService],
-  exports: [AppConfigService],
-})
-export class AppConfigModule {}
+@Module({})
+export class AppConfigModule {
+  static forRoot(): DynamicModule {
+    return {
+      module: AppConfigModule,
+      providers: [AppConfigService],
+      exports: [AppConfigService],
+    };
+  }
+}
